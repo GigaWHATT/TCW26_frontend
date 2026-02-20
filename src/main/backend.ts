b77f3e9250bc5d899b8e1027cdff361ea5342846
Etienne <guegan.etienne@gmail.com>
2026-02-19 21:55:53 -0500

    download backend from git

--- src/main/backend.ts
+++ src/main/backend.ts
@@ -1,9 +1,11 @@
 import * as os from 'os';
+import * as https from 'https';
+import * as fs from 'fs';
 
 const opsys: string = os.type();
 const archsys: string = os.arch();
 
-console.log(opsys,archsys)
+console.log(opsys,archsys);
 
 function getBackendFileName() : string {
     if (opsys === 'Windows_NT' && archsys === 'x64') return 'TCW26_backend-0.0.0-windows-x86_64.exe';
@@ -14,5 +16,23 @@ function getBackendFileName() : string {
     throw new Error(`Unsupported OS/arch combination: ${opsys} ${archsys}`);
 }
 
-console.log(getBackendFileName())
+console.log(getBackendFileName());
 
+const fileName: string = getBackendFileName();
+
+const url: string ='https://github.com/GigaWHATT/TCW26_backend/releases/download/v0.0.0/'+fileName;
+const filePath: string ='../../backend_builds/'+fileName;
+const file = fs.createWriteStream(filePath);
+
+function downloadFile(): void{
+    https.get(url, (response) => {
+        response.pipe(file);
+
+        file.on('finish',() => {
+            file.close();
+            console.log('Downloaded backend.');
+        });
+    });
+}
+
+downloadFile()
